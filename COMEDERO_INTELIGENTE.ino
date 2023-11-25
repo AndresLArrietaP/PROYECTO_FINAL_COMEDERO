ae5dc98bcf7c6abf0a4e65f49d3088a7aeea8113
AndyArrieta <andrestransformers@hotmail.com>
2023-11-24 22:09:26 -0500

    proyecto
    G5

---
+++ COMEDERO_INTELIGENTE.ino
@@ -0,0 +1,53 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <Servo.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+Servo myservo;
+static const int servoPin = 13;
+const char* ssid     = "TU-WIFI";
+const char* password = "TU-PASSWORD-WIFI";
+#define BOTtoken "TU-TOKEN-BOT"
+#define CHAT_ID "TU-CHAT-ID"
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+void handleNewMessages(int numNewMessages) {
+  for (int i=0; i<numNewMessages; i++) {
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Usuario no autorizado", "");
+      continue;
+    }
+    String text = bot.messages[i].text;
+    if (text == "/comida") {
+      bot.sendMessage(chat_id, "Alimentando", "");
+      myservo.write(90);             
+      delay(500);                       
+      myservo.write(0);              
+    }
+  }
+}
+void setup() {
+  Serial.begin(115200);
+  myservo.attach(servoPin);
+  Serial.print("Conectado a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  } 
+  Serial.println("");
+  Serial.println("WiFi conectado.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  bot.sendMessage(CHAT_ID, "Bot iniciado", "");
+}
+void loop() {
+  int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  while(numNewMessages) {
+    handleNewMessages(numNewMessages);
+    numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  }
+}
